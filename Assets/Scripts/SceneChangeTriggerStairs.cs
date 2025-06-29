1a69cc166a917710831cff411716bba333be9eb6
Minh Thi Nguyen <minhthi.ngyn@gmail.com>
2025-06-30 01:04:38 +0200

    StartScreen
    Finished all StartscreenUI, connected Play to next Scene

--- Assets/Scripts/SceneChangeTriggerStairs.cs
+++ Assets/Scripts/SceneChangeTriggerStairs.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class SceneChangeTrigger : MonoBehaviour
+public class SceneChangeTriggerStairs : MonoBehaviour
 {
     public string sceneToLoad; 
 
